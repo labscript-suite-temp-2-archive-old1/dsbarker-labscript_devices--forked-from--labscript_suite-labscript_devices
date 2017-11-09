87c61ab032c4ba0747efdcccd48849c9cc4fb193
Daniel Barker <dsbark7@gmail.com>
2017-11-09 15:18:31 -0500

    add_GP370: Fixed Blacs
    that's it
    
    spe

--- GP370.py
+++ GP370.py
@@ -17,7 +17,7 @@ import numpy as np
 import visa
 
 @labscript_device
-class GP370Controller(Device):    
+class GP370(Device):    
     @set_passed_properties(property_names = {"connection_table_properties" : ["visa_resource"]})
     def __init__(self, name, visa_resource = ""):
         Device.__init__(self, name, None, None)
@@ -27,12 +27,18 @@ class GP370Controller(Device):
         pass
         
 
-import time
+import os
+
+from qtutils.qt.QtCore import *
+from qtutils.qt.QtGui import *
 
 from blacs.tab_base_classes import Worker, define_state
 from blacs.tab_base_classes import MODE_MANUAL, MODE_TRANSITION_TO_BUFFERED, MODE_TRANSITION_TO_MANUAL, MODE_BUFFERED  
 
 from blacs.device_base_class import DeviceTab
+
+from qtutils import UiLoader
+import qtutils.icons
 
 @BLACS_tab
 class GP370ControllerTab(DeviceTab):
@@ -43,8 +49,8 @@ class GP370ControllerTab(DeviceTab):
         self.ui = UiLoader().load(ui_filepath)
         layout.addWidget(self.ui)
         
-        visa_resource =  self.settings['connection_table'].find_by_name(self.settings["visa_resource"]).BLACS_connection
-        self.ui.visa_resource_label.setText(visa_resource) 
+        self.visa_resource =  self.settings['connection_table'].find_by_name(self.device_name).BLACS_connection
+        self.ui.visa_resource_label.setText(self.visa_resource) 
         
         self.ui.check_connectivity_pushButton.setIcon(QIcon(':/qtutils/fugue/arrow-circle'))
         self.ui.check_connectivity_pushButton.clicked.connect(self.check_connection_and_read_pressure) 
@@ -58,17 +64,17 @@ class GP370ControllerTab(DeviceTab):
         self.ui.connection_status.setText(status_text)
         
         value = yield(self.queue_work(self.primary_worker, 'read_pressure'))
-        self.ui.current_pressure(value)
+        self.ui.current_pressure.setText("{0:.2E}".format(value))\
         
         status_text = 'Working!'
         icon = QIcon(':/qtutils/fugue/tick')
         pixmap = icon.pixmap(QSize(16, 16))
         self.ui.status_icon.setPixmap(pixmap)
-        elf.ui.connection_status.setText(status_text)
+        self.ui.connection_status.setText(status_text)
         
     def initialise_workers(self):
         # Create and set the primary worker
-        self.create_worker("main_worker",GP370Worker,{'com_port':self.visa_resource})
+        self.create_worker("main_worker",GP370Worker,{'visa_resource':self.visa_resource})
         self.primary_worker = "main_worker"
 
 @BLACS_worker    
@@ -87,22 +93,25 @@ class GP370Worker(Worker):
         # while response is not None:
         #    response = self.connection.query()
     
+    def program_manual(self,values):
+        pass
+    
     def read_pressure(self):
         return float(self.connection.query('DS IG'))
         # TODO: some basic error checking
     
-    def transition_to_buffered(self,device_name,h5file,initial_values,fresh):
-        with h5py.File(self.h5_file,'a') as hdf5_file:
+    def transition_to_buffered(self,device_name,h5file):
+        return True
+    
+    def transition_to_manual(self,device_name,h5file):
+        with h5py.File(self.h5file,'a') as hdf5_file:
             try:
                 gp370data = hdf5_file['/data/'+ self.device_name]
             except:
                 # Group doesn't exist yet, create it:
-                gp370data = hdf5_file.create_group('/data/GP370')
+                gp370data = hdf5_file.create_group('/data/' + self.device_name)
                 
             gp370data.createdataset('pressure',data=self.read_pressure())
-    
-    def transition_to_manual(self):
-        return True
     
     def abort_buffered(self):
         return True
